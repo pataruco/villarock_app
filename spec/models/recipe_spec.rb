c132f95669c2d6216ea46f1ae4d859bd130b3ed5
Pedro Martin <pataruco@gmail.com>
2015-06-08 15:01:46 +0100

    rspec for recipe has a description passed

--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -1,8 +1,18 @@
 require 'rails_helper'
 
 describe Recipe do
-    it "must have a name" do
+  it "must have a Name" do
     r1 = Recipe.create
     expect(r1.valid?).to eq false
   end
-end
+
+  it "must have a Description" do
+    r1 = Recipe.create
+    expect(r1.valid?).to eq false
+  end
+
+
+
+
+
+end # End of recipe
