85056c6e9fa0fd07d2c50eb46f04e5e699921248
Pedro Martin <pataruco@gmail.com>
2015-06-08 17:11:08 +0100

    Seeding done

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,5 +23,26 @@ end
 
 ingredients_seeder
 
-# i1 = Ingredient.create(name:'Tomato', stock: true)
-# i2 = Ingredient.create(name:'Olive oil', stock: true)
+tomato = Ingredient.create(name:'Tomato', stock: true)
+olive_oil = Ingredient.create(name:'Olive oil', stock: true)
+mozzarella = Ingredient.create(name:'Mozzarella cheese', stock: false)
+sauce = Ingredient.create(name:'Tomato sauce', stock: false)
+
+def recipe_seeder
+  recipes = ['Ceasar salad', 'Capresa salad', 'Green salad', 'Choripan', 'Lemon chicken', 'Chicken fajitas', 'Thai green chicken curry', 'Honey-mustard chicken', 'Shread dry meat', 'Roast beef', 'burguers', 'Spinach soup', 'Lentils', 'Stir & Fry', 'Black bean fajitas', 'Potato tortilla', 'Aurbigne lasagna', 'Rissotto', 'Pancakes']
+  recipes.each do |recipe|
+    Recipe.create(name: recipe, preparation: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque ex et ratione voluptates iusto perspiciatis dolore aperiam rem vel. Alias culpa vero beatae dolorum blanditiis sequi iusto at obcaecati reiciendis.')
+  end
+end
+
+recipe_seeder
+
+pizza = Recipe.create(name: 'Pizza', preparation: 'Top the Pizza: Spoon a few tablespoons of sauce into the center of the pizza and use the back of a spoon to spread it out to the edges. Pile on all of your toppings. Bake the Pizza: Using a pizza peel or the backside of a baking sheet, slide your pizza (with the parchment or with the cornmeal) onto the baking stone. If you dont have a baking stone, bake the pizza right on the baking sheet. Cook for 5 minutes, then rotate the pizza. Slide the parchment out from under the pizza, if used. Bake for another 3-5 minutes, until the crust is golden-brown and the cheese looks toasty. Slice and Serve: Let the pizza cool slightly on a cooling rack, just until youre able to handle it. Slice it into pieces and serve.')
+
+pizza.ingredients << tomato << olive_oil << mozzarella << sauce
+
+pizza_lunch = Meal.create(date: Time.now, category: 1)
+pizza_lunch.recipe = pizza
+
+
+
