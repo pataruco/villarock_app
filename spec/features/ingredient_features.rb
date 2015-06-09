ed2574de60f87b98f5995321587d431c79625a6e
Pedro Martin <pataruco@gmail.com>
2015-06-09 13:11:21 +0100

    Ingredients show page showing stock/to buy

--- spec/features/ingredient_features.rb
+++ spec/features/ingredient_features.rb
@@ -3,7 +3,8 @@ require 'rails_helper'
 describe 'adding an ingredient' do
 
   it 'a user can add an ingredient to the site' do
-    visit '/ingredients/new'  
+    # visit '/ingredients/new'  
+    visit new_ingredient_path
     fill_in 'Name', with: 'Basil'
     click_button 'Submit'
 
