eaa5b84065bfb3a0c0c5da7b607ba51f9d7a2869
Pedro Martin <pataruco@gmail.com>
2015-06-10 20:12:03 +0100

    Add normalize to the stylesheet

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,7 +1,7 @@
 class Meal < ActiveRecord::Base
   has_one :recipe
   
-  :date
-  #validates :date, presence: true
+  #:date
+  validates :date, presence: true
   enum category: [ :breakfast, :lunch, :dinner ]
 end
