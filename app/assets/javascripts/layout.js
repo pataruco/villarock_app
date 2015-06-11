58154a425d5c1b08dc4870e4cbb562857e031969
Pedro Martin <pataruco@gmail.com>
2015-06-11 15:34:08 +0100

    Buttons on ingredients done and styled

--- app/assets/javascripts/layout.js
+++ app/assets/javascripts/layout.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
   $('nav li ul').hide().removeClass('fallback');
   $('nav li').hover(
-  function () {
-    $('ul', this).stop().slideDown(100);
-  },
-  function () {
-    $('ul', this).stop().slideUp(100);
-  }
+    function () {
+      $('ul', this).stop().slideDown(100);
+    },
+    function () {
+      $('ul', this).stop().slideUp(100);
+    }
 );
 })
