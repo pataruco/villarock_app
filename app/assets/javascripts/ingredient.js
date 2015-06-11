b1fb4b5c620954e312ee0cfeed697f58f6886ffd
Pedro Martin <pataruco@gmail.com>
2015-06-11 17:45:44 +0100

    Buttons on ingredients working / Disable turbolinks on JS

--- app/assets/javascripts/ingredient.js
+++ app/assets/javascripts/ingredient.js
@@ -1,39 +1,39 @@
-$(document).ready(function(){
-  console.log('hi');
-  // variables
-  var StockList = $('.ingredients_stock');
-  var ToBuyList = $('.ingredients_to_buy');
-  var ButtonStock = $('.in_stock');
-  var ButtonBuy = $('.buy');
-  var IngredientList = $('.ingredients_list');
-   // On load
-  StockList.hide();
-  ToBuyList.hide();
-  // Event listener
-  ButtonStock.on('click', ShowStockList);
-  ButtonBuy.on('click', ShowBuyList);
+// $(document).ready(function(){
+//   console.log('hi');
+//   // variables
+//   var StockList = $('.ingredients_stock');
+//   var ToBuyList = $('.ingredients_to_buy');
+//   var ButtonStock = $('.in_stock');
+//   var ButtonBuy = $('.buy');
+//   var IngredientList = $('.ingredients_list');
+//    // On load
+//   StockList.hide();
+//   ToBuyList.hide();
+//   // Event listener
+//   ButtonStock.on('click', ShowStockList);
+//   ButtonBuy.on('click', ShowBuyList);
 
 
-  //Functions
+//   //Functions
 
-  function ShowStockList () {
-    ToBuyList.hide();
-    IngredientList.hide();
-    StockList.show();
-  }
+//   function ShowStockList () {
+//     ToBuyList.hide();
+//     IngredientList.hide();
+//     StockList.show();
+//   }
 
-  function ShowBuyList () {
-    StockList.hide();
-    IngredientList.hide();
-    ToBuyList.show();
-  }
-
-
-
+//   function ShowBuyList () {
+//     StockList.hide();
+//     IngredientList.hide();
+//     ToBuyList.show();
+//   }
 
 
 
 
 
 
-}) // End of document ready
+
+
+
+// }) // End of document ready
--- app/assets/javascripts/layout.js
+++ app/assets/javascripts/layout.js
@@ -1,4 +1,45 @@
 $(document).ready(function(){
+  console.log('hi');
+  // variables
+  var ButtonIngredientNav = $('.ButtonIngredientNav');
+  var StockList = $('.ingredients_stock');
+  var ToBuyList = $('.ingredients_to_buy');
+  var ButtonStock = $('.in_stock');
+  var ButtonBuy = $('.buy');
+  var IngredientList = $('.ingredients_list');
+   // On load
+  StockList.hide();
+  ToBuyList.hide();
+  // Event listener
+  ButtonIngredientNav.on('click', ShowIngredientsPage)
+  ButtonStock.on('click', ShowStockList);
+  ButtonBuy.on('click', ShowBuyList);
+
+
+  //Callbacks on ingredients
+
+  function ShowIngredientsPage () {
+    console.log('ShowIngredientsPage')
+    // $(document).reload();
+    StockList.hide();
+    ToBuyList.hide();
+  }
+
+  function ShowStockList () {
+    ToBuyList.hide();
+    IngredientList.hide();
+    StockList.show();
+  }
+
+  function ShowBuyList () {
+    StockList.hide();
+    IngredientList.hide();
+    ToBuyList.show();
+  }
+
+
+
+  // Styling the nav
   $('nav li ul').hide().removeClass('fallback');
   $('nav li').hover(
     function () {
@@ -7,5 +48,5 @@ $(document).ready(function(){
     function () {
       $('ul', this).stop().slideUp(100);
     }
-);
-})
+  );
+}) // End document ready
