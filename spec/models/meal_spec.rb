18545105dbc10bffeb23a75870978d4c7ee76e7f
Pedro Martin <pataruco@gmail.com>
2015-06-11 10:56:32 +0100

    rspec is fixed

--- spec/models/meal_spec.rb
+++ spec/models/meal_spec.rb
@@ -11,14 +11,7 @@ describe Meal do
     expect(meal.valid?).to eq false
   end
 
-  context 'have a recipe' do
-    it 'should return the name of the recipe' do
-      m1 = Meal.create(date: DateTime.new)
-      r1 = Recipe.create(name: 'Lasagna')
-      m1.recipe = r1
-      expect(m1.recipe.name).to eq 'Lasagna'
-    end
-  end
+
 
 
 
